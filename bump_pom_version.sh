c566ab5870c4aafca95a3df5c49d05057dd83cfa
foliet <1843619244@qq.com>
2024-04-09 14:09:55 +0800

    Update bump_pom_version.sh

--- bump_pom_version.sh
+++ bump_pom_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-version=$(grep -oP '(?<=^\t<version>).*?(?=<\/version>)' pom.xml)
+version=$(perl -n -e 'if (/^( {4}|\t)<version>(.*?)<\/version>/) { print $2 }' pom.xml)
 major_minor=$(echo "$version" | grep -Eo '[0-9]+\.[0-9]+\.')
 patch=$(echo "$version" | grep -oP "(?<=${major_minor}).*")
 next_patch=$((patch + 1))
 new_version=$(echo "$major_minor$next_patch")
-sed -i "s/^\t<version>${version}<\/version>/\t<version>${new_version}<\/version>/" pom.xml
+sed -i "s/^\(\t\| \{4\}\)<version>${version}<\/version>/\1<version>${new_version}<\/version>/" pom.xml
